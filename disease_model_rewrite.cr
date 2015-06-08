6c55b45ee353cc56fb7943d0e3ae6b4762fb86ac
Dr.Thunder <thunderheavyindustries@gmail.com>
2015-06-07 18:04:56 -0600

    randomized gendering added, promiscuity randomness added

--- disease_model_rewrite.cr
+++ disease_model_rewrite.cr
@@ -14,7 +14,7 @@ class Agent
 
   getter id
 
-  getter mating_threshold
+  getter promiscuity
 
   getter gender
 
@@ -23,9 +23,9 @@ class Agent
 
   getter sexuality
 
-  def initialize(id, mating_threshold, gender, infected, sexuality, when_infected)
+  def initialize id, promiscuity, gender, infected, sexuality, when_infected 
     @id = id
-    @mating_threshold = mating_threshold
+    @promiscuity = promiscuity
     @gender = gender
     @infected = infected
     @sexuality = sexuality
@@ -38,33 +38,44 @@ end
 # set up the simulation
 simulation_runs = 0 #how many times the simulation runs
 transmission_probability = 0.64 #sets the probability of individual getting the disease if they mate.
-mating_threshold = 0.6 #Likely hood that a given pair mate, combined mating score needs to beat this threshold
+mating_threshold = 0.6 #Likely hood that a given pair mate, combined mating score needs to beat this threshold (think of it as how conservative in it's sexual practices the society is at large)
 save_previous_state = false #the infected from the previous generation informs the current generation.
-GenderRatio = [0,0] # [ladies, men]
-population = [] of Agent #this is our array of agents from the agent class 
+gender_ratio =  0.4 # percentage of the population that is different from the other
 
 
 
-def create_population population_size, starting_infected, mating_threshold, gender_ratio
+def create_population population_size, starting_infected, gender_ratio
 
-  #agent initial = id, mating_threshold, gender, infected, sexuality, when_infected)
+  population = [] of Agent #this is our array of agents from the agent class
+  gender = ""
+
 
   (0..population_size-1).each do |n|
 
-    if starting_infected>0
-      agent = Agent.new n , n+10, "m", true, "g", 0
-      population.push(agent)
+    if rand > gender_ratio
+      gender = "male"
     else
-      
-     agent = Agent.new n , n+10, "m", false, "g", 0
+      gender = "female"
+    end
+
+    if starting_infected>0
+
+      agent = Agent.new n , rand, gender, true, "g", 0
+      population.push(agent)
+      starting_infected = starting_infected-1
+    else
+
+     agent = Agent.new n , rand, gender, false, "g", 0
      population.push(agent)
    end
   end
-
-
+  return population
 end
 
 
+p = create_population 200, 5, 0.5
+
+(0..25).each {|n| puts " #{p[n].id}, #{p[n].gender}, #{p[n].promiscuity}% , #{p[n].infected}" }
 
 
 # For speed checking purposes
